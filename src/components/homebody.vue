66ab3eb8f15934096272be2198cf39081a99cd75
Nathan Ho <nho21@uic.edu>
2021-03-10 22:56:01 -0600

    firebase clutch, fixed up some writings

--- src/components/homebody.vue
+++ src/components/homebody.vue
@@ -3,15 +3,26 @@
         <h3>
             {{ msg }}
         </h3>
-        <h4>
+       <br>
+       <h4>
             Stanford Treehacks (hackathon)
         </h4>
         <p>
             During the Treehacks hackathon, I learned and experimented with multiple machine learning principles. The project my group
-            worked on was an app that would help a user determine a general risk level of contracting COVID-19 in the user's location.
+            worked on was a study on the COVID-19 cases in New York City. With the data in hand, we studied and used statistical analyses to find correlation in 
+            COVID-19 cases and public transportation throughout the five boroughs. 
         </p>
-        <br>
+
         <a href="https://devpost.com/software/covid-19-travel-app" target="_blank" rel="noopener"> Devpost Posting </a>
+        <h4>
+          Chicago data app
+        </h4>
+        <p>
+          The purpose of this app is to advise users of their surroundings and overall allow safer commuting around Chicago. The app will periodically inform users of the crime frequency 
+          and most recurring category of crime around a given destination or their own given location.
+        </p>
+
+        <a href="https://github.com/nkh361/chicago-crime-analyses" target="_blank" rel="noopener"> Repository link </a>
     </div>
 </template>
 
--- vue.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    publicPath: '/nkh361.github.io/'
-  }
